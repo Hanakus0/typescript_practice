c2ed3d0d1065a18c40d4b1a3a2b23f2b280ce870
Hanakus0 <everydoghas12day@outlook.jp>
2025-01-15 21:48:04 +0900

    20250115

---
+++ 02_compiler/01_watch.ts
@@ -0,0 +1,9 @@
+let hello : string = 'hello';
+console.log(hello);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+/* エラー */
+// let hello :number = 'hello';
---
+++ 02_compiler/tmp/files/compiler.js
@@ -0,0 +1,5 @@
+"use strict";
+// 以下設定によりコンパイル対象外
+// "files": [
+//   "tmp/compiler.ts"
+// ]
