2fd63316f76864a3e0e8e83d2341be246577a946
Hanakus0 <everydoghas12day@outlook.jp>
2025-02-02 22:16:27 +0900

    20250202

--- 05_advanced/src/lookup.js
+++ 05_advanced/src/lookup.js
@@ -1,7 +1,14 @@
 "use strict";
-var _a, _b, _c;
 const data = {
     id: 1,
+    user: {
+        name: {
+            first: 'Taro',
+            last: 'Yamada'
+        }
+    }
 };
-console.log((_b = (_a = data.user) === null || _a === void 0 ? void 0 : _a.name) === null || _b === void 0 ? void 0 : _b.first);
-const userDate = (_c = data.user) !== null && _c !== void 0 ? _c : 'no-user';
+const strAry = [
+    'first',
+    'last'
+];
--- 05_advanced/ts/lookup.ts
+++ 05_advanced/ts/lookup.ts
@@ -1,6 +1,6 @@
 interface DownloadedDataInfo {
   id: number;
-  user?: {
+  user: {
     name?: {
       first: string;
       last: string;
@@ -10,9 +10,27 @@ interface DownloadedDataInfo {
 
 const data: DownloadedDataInfo = {
   id: 1,
+  user: {
+    name: {
+      first: 'Taro',
+      last: 'Yamada'
+    }
+  }
 }
 
-console.log(data.user?.name?.first);
+const strAry: string[] = [
+  'first',
+  'last'
+]
 
-const userDate = data.user ?? 'no-user';
-type id = DownloadedDataInfo['id']; // ブラケット記法
+// ブラケット記法 -> T[K]
+type id = DownloadedDataInfo['id'];
+type userName = DownloadedDataInfo['user']['name'];
+
+// データ型の取得に際してもユニオン型で取得可能
+type unionType = DownloadedDataInfo['id' | 'user'];
+
+// 配列の中身を取得する場合
+type strAryType = string[];
+type aryVal1 = strAryType[0]; // 0以外を入れてもエラーにならない
+type aryVal2 = strAryType[number]; // numberを指定することで配列の中身を取得できる
